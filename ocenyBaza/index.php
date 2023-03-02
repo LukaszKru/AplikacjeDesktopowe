e6a784697015689c538534f47bb15b92ad5201ed
LukaszKru <97897234+LukaszKru@users.noreply.github.com>
2023-03-02 08:42:30 +0100

    Add files via upload

--- ocenyBaza/index.php
+++ ocenyBaza/index.php
@@ -20,38 +20,53 @@
                 $przedmiot[]=$wiersz['nazwa'];
                 $idprzedmiot[]=$wiersz['id'];
             }
-        }else{
-            echo "nie gut";
-        }
-        if($connect){
-            $zapytanie1="SELECT * FROM dane";
-            $wynik=mysqli_query($connect,$zapytanie1);
+            $zapytanie="SELECT * FROM dane";
+            $wynik=mysqli_query($connect,$zapytanie);
             while($wiersz=mysqli_fetch_array($wynik)){
                 $iduczen[]=$wiersz['id'];
                 $imie[]=$wiersz['imie'];
                 $nazwisko[]=$wiersz['nazwisko'];
             }
-        }else{
-            echo "nie gut";
+            if($_SERVER["REQUEST_METHOD"]=="POST"){
+                $przedmiotID=$_POST["przedmiot"];
+                $osoba=$_POST["uczen"];
+                $ocena=$_POST["ocena"];
+                $insert="INSERT INTO oceny VALUES (NULL,'$osoba','$przedmiotID','$ocena')";
+                if(mysqli_query($connect,$insert)){
+                    echo "Dodano dane do bazy";
+                }else{
+                    echo "Bład dodawania do bazy";
+                }
+            }
+        else{
+                echo "nie gut";
+        }
         }
         mysqli_close($connect);
         ?>
-        <label for="przedmiot">Wybierz przedmiot</label>
-        <select name="przedmiot" id="przedmiot">
-            <?php 
-                for($i=0;$i<count($przedmiot);$i++){
-                    echo "<option value=$idprzedmiot[$i]> $przedmiot[$i]</option>";
-                }
-            ?>
-        </select>
-        <label for="uczen">Wybierz ucznia</label>
-        <select name="uczen" id="uczen">
-            <?php 
-                for($i=0;$i<count($iduczen);$i++){
-                    echo "<option value=$iduczen[$i]> $imie[$i] $nazwisko[$i]</option>";
-                }
-            ?>
-        </select>
+        <form method="POST">
+            <label for="przedmiot">Wybierz przedmiot</label>
+            <select name="przedmiot" id="przedmiot">
+                <?php 
+                    for($i=0;$i<count($przedmiot);$i++){
+                        echo "<option value=$idprzedmiot[$i]> $przedmiot[$i]</option>";
+                    }
+                ?>
+            </select>
+            <label for="uczen">Wybierz ucznia</label>
+            <select name="uczen" id="uczen">
+                <?php 
+                    for($i=0;$i<count($iduczen);$i++){
+                        echo "<option value=$iduczen[$i]> $imie[$i] $nazwisko[$i]</option>";
+                    }
+                ?>
+            </select>
+            <br>
+            <label for="ocena">Wpisz ocene</label>
+            <input type="number" id="ocena" name="ocena">
+            <br>
+            <input type="submit" value="Zapisz do bazy">
+        </form>
     </main>
     <footer>
         <p>Stronę wykonał Łukasz Krupa</p>
